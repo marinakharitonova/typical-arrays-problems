3df6e89931047045bb59471fadc4753ef320a1ab
marinakharitonova <6172331@mail.ru>
2020-09-28 19:59:03 +0300

    feat: add solution to typical-array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length > 0){
+        let min = 0;
+        for (let item of array){
+            if (item < min){
+                min = item;
+            }
+        }
+        return min;
+    } else return 0;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0){
+        let max = 0;
+        for (let item of array){
+            if (item > max){
+                max = item;
+            }
+        }
+        return max;
+    } else return 0;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length > 0){
+        let avg = 0;
+        for (let item of array){
+            avg += item;
+        }
+        return avg / array.length;
+    } else return 0
+
 }
